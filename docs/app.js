da58cf8170d27005bb93aefac491b80051b3dc59
leonardoarielcostante <leonardoarielcostante@gmail.com>
2023-01-11 18:40:06 -0300

    Primer commit

---
+++ docs/app.js
@@ -0,0 +1,56 @@
+const txts= document.querySelector(".contenedor-frases").children;
+         txtsLen= txts.length;
+let   index= 0;
+
+
+var myCarousel = document.getElementById('carouselExampleSlidesOnly')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+    for(let i=0 ; i < txtsLen ; i++){
+                     txts[i].classList.remove("text-in");
+                 }
+    txts[index].classList.add("text-in");
+             if(index == txtsLen-1){
+                 index= 0;
+             } else {
+                 index++;
+             }
+})
+
+
+const nav = document.getElementById("navbar-scroll")
+
+
+window.addEventListener('scroll', function (){
+    const offset = window.pageYOffset;
+
+    if(offset > 235) {
+      nav.classList.add('scroll')
+    }else {
+        nav.classList.remove('scroll') 
+    }
+});
+
+
+
+// const txts= document.querySelector(".contenedor-frases").children;
+//         txtsLen= txts.length;
+// let   index= 0;
+        
+//     function animatedText() {
+//         for(let i=0 ; i < txtsLen ; i++){
+//             txts[i].classList.remove("text-in");
+//         }
+//         txts[index].classList.add("text-in");
+//         if(index == txtsLen-1){
+//             index= 0;
+//         } else {
+//             index++;
+//         }
+
+//         setTimeout(animatedText, 3000);
+//     }
+
+//     window.onload=animatedText;
+
+    
